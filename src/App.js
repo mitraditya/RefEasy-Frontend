c227affaf9ef4b0dd5ea0bc4990be66312acdd38
Souparno Paul <souparnopaul@gmail.com>
2022-05-19 22:00:57 +0530

    1. Added Material UI 2. Added Login and Register Components 3. Added React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./TI-logo.png";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { Container, Link, Box, CssBaseline, Grid } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[700],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Box
+          component="img"
+          src={logo}
+          maxWidth={{ xs: 160 }}
+          maxHeight={{ xs: 90 }}
+        />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Container maxWidth="sm">
+                  <div>
+                    Welcome to the Home Page! Visit the Login or Register page
+                    by clicking on the links below.
+                  </div>
+                  <Grid container sx={{ mt: 2 }}>
+                    <Grid item xs>
+                      <Link href="/login">Login</Link>
+                    </Grid>
+                    <Grid item xs>
+                      <Link href="/register">Register</Link>
+                    </Grid>
+                  </Grid>
+                </Container>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
